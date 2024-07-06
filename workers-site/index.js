4041081d7010ca7b5b1b7b85bb76c5119af6d0ad
santosh-nairiti <santosh@nairiti.com>
2024-07-06 12:09:32 +0530

    Update index.js

--- workers-site/index.js
+++ workers-site/index.js
@@ -3,29 +3,7 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
-  // Replace 'b46b49b26e5b4fc09913fd35b61ccb46' with your actual Workers KV namespace ID
-  const myKVNamespace = await KV.get('b46b49b26e5b4fc09913fd35b61ccb46');
-  
-  // Determine which file to fetch based on request path
-  let key, contentType;
-  if (request.url.endsWith('/scripts.min.js')) {
-    key = 'js/scripts.min.9be8381057.js';
-    contentType = 'application/javascript';
-  } else if (request.url.endsWith('/style.min.css')) {
-    key = 'css/style.min.24e03f9a96.css';
-    contentType = 'text/css';
-  } else {
-    // Handle other files or return 404 if not found
-    return new Response('File not found', { status: 404 });
-  }
-  
-  // Fetch the value from KV store
-  const value = await myKVNamespace.get(key, 'text');
-  
-  // Return the response with the content and appropriate headers
-  return new Response(value, {
-    headers: {
-      'Content-Type': contentType,
-    },
-  });
+  return new Response('Hello, World!', {
+    headers: { 'content-type': 'text/plain' },
+  })
 }
